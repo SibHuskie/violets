209d3d6c4ada22b7eb7d0187bb61c98c55a88b56
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-04-16 19:03:33 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -669,4 +669,35 @@ async def ship(ctx, userName1: discord.Member = None, userName2: discord.Member 
         else:
             msg.add_field(name=":heartpulse: S H I P  M A C H I N E :heartpulse: ", value=":small_red_triangle_down: **{}**\n:small_red_triangle: **{}**\n```fix\n{}% - Perfect\n```\n:revolving_hearts: ".format(userName1.display_name, userName2.display_name, percent))
     await client.say(embed=msg)
+    
+@client.command(pass_context=True)
+async def warn(ctx, userName: discord.Member = None, *, args = None):
+    helper_role = discord.utils.get(ctx.message.server.roles, name='Helper')
+    mod_role = discord.utils.get(ctx.message.server.roles, name='Moderators')
+    admin_role = discord.utils.get(ctx.message.server.roles, name='Administrator')
+    manager_role = discord.utils.get(ctx.message.server.roles, name='Co Owner')
+    owner_role = discord.utils.get(ctx.message.server.roles, name='Owners')
+    author = ctx.message.author
+    msg = discord.Embed(colour=0x871485, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    msg2 = discord.Embed(colour=0x871485, description= "")
+    msg2.title = ""
+    msg2.set_footer(text=footer_text)
+    if helper_role in author.roles or mod_role in author.roles or admin_role in author.roles or manager_role in author.roles or owner_role in author.roles:
+        if userName == None or args == None:
+            msg.add_field(name=":warning: ", value="`<warn (user) (reason)`")
+            await client.say(embed=msg)
+        else:
+            if helper_role in userName.roles or mod_role in userName.roles or admin_role in userName.roles or manager_role in userName.roles or owner_role in userName.roles:
+                msg.add_field(name=":warning: ", value="`You cannot warn other staff!`")
+                await client.say(embed=msg)
+            else:
+                msg2.add_field(name=":pencil: ", value="`You have been warned by {} in The Realm Of Darkness!`\n`Reason: {}`".format(author.display_name, args))
+                msg.add_field(name=":pencil: ", value="`{} warned {}!`\n`Reason: {}`".format(author.display_name, userName.display_name, args))
+                await client.say(embed=msg)
+                await client.send_message(userName, embed=msg2)
+    else:
+        msg.add_field(name=":warning: ", value="`This command can only be used by staff!`")
+        await client.say(embed=msg)
 client.run(os.environ['BOT_TOKEN'])
