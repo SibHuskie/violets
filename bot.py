df31c3777270e28ef8967e71d98fd2940573a096
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-06 10:14:38 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("---------------")
-    await client.change_presence(game=discord.Game(name='wit a God (Huskie)'))
+    await client.change_presence(game=discord.Game(name='on Violets™'))
 
 @client.command()
 async def testing():
