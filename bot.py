310419710c4f6477bd069afc593f0de0ba8b756b
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-06 16:16:28 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -832,4 +832,16 @@ async def on_message(message):
         if word.upper() in husk_filter:
             if not message.author.id in bypass_list:
                 await client.send_message(message.channel, ":wolf: ")
+                
+word_filter = ["NIGGER", "NIGGA"]
+bypass_list = []
+
+@client.event
+async def on_message(message):
+    contents = message.content.split(" ")
+    for word in contents:
+        if word.upper() in word_filter:
+            if not message.author.id in bypass_list:
+                await client.delete_message(message)
+                await client.send_message(message.channel, "**Nope**")
 client.run(os.environ['BOT_TOKEN'])
