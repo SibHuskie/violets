030f85af0f1044cfed42580d8ebe8dac86a46d08
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-23 21:35:40 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1109,4 +1109,9 @@ async def on_message(message):
         await client.send_message(message.channel, 'Say hello')
         msg = await client.wait_for_message(author=message.author, content='hello')
         await client.send_message(message.channel, 'Hello.')
+        
+@client.event
+async def on_message(message):
+    if message.content.startswith('gay'):
+        await client.send_message(message.channel, 'no u')
 client.run(os.environ['BOT_TOKEN'])
