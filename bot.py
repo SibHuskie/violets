32f35e1be09d8057e9a517d0c3db4b9cb92331dd
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-24 21:27:15 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1111,10 +1111,7 @@ async def on_message(message):
     await client.process_commands(message)
     if message.content.lower().startswith('who is huskie'):
         await client.send_message(message.channel, "Da best")
-        
-@client.event
-async def on_message(message):
-    await client.process_commands(message)
+
     if message.content.lower().startswith('v!coin'): #Coinflip 50/50% chance kopf oder zahl
         choice = random.randint(1,2)
         if choice == 1:
