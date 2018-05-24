e445f05d17b7e0bc8224a946ec449551eaa9027e
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-05-24 19:43:29 +1000

    Update convo.py

--- convo.py
+++ convo.py
@@ -8,10 +8,10 @@ import os
 
 @client.event
 async def on_message(message):
-    if message.content.lower().startswith('?test'):
-        await client.send_message(message.channel, "Test bestanden")
+    if message.content.lower().startswith('v!test'):
+        await client.send_message(message.channel, "Testing 1 2 3...")
 
-    if message.content.lower().startswith('?coin'): #Coinflip 50/50% chance kopf oder zahl
+    if message.content.lower().startswith('v!coin'): #Coinflip 50/50% chance kopf oder zahl
         choice = random.randint(1,2)
         if choice == 1:
             await client.add_reaction(message, '🌑')
