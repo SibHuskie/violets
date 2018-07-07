ebcea4e0df4c632d7e838f605ee2f1369c4eaf53
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-07-07 17:31:58 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1551,6 +1551,7 @@ async def howgay(ctx, user: discord.Member = None):
 @client.command(pass_context=True)
 async def cb(ctx):
     staff_role = discord.utils.get(ctx.message.server.roles, name='Staff')
+    chnl = ctx.message.channel
     author = ctx.message.author
     msg = discord.Embed(colour=0x871485, description= "")
     msg.title = ""
