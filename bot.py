55584968ada81db166e83619b2036bcda6e9a99d
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-07-19 13:15:36 +1000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,209 @@
+import discord
+from discord.ext import commands
+import logging
+import asyncio
+import random
+import time
+import os
+import os.path
+import requests
+import json
+import urbandictionary as ud
+
+client = commands.Bot(command_prefix="v!")
+footer_text = "Violets™"
+error_img = ':warning:'
+default_invite = 'https://discord.gg/GnkADTA'
+
+#Welcome and Leave
+
+@client.async_event
+async def on_member_join(userName: discord.User):
+    m2 = "Welcome to **Violets**, <@{}>! We hope you enjoy your stay and have fun.".format(userName.id)
+    m2 += "\nAll information is in the <#426683264682557440> channel, but feel free to ask the staff about anything you want to know."
+    m2 += "\https://gph.is/2lnKhvK"
+    m = "\n**Welcome to Violets™ <@{}>! :sparkles:".format(userName.id)
+    m += "\nPlease make sure to read the rules and if you want to partner, contact any of the staff with the role Partnership Manager :smiley:"
+    m += "\nAlso don't forget to get roles and colors in the <#440562714989821982> and the <#427124007377305611> channels :wink: \nEnjoy your stay :sparkling_heart:**"
+    m += "\nhttps://gph.is/2lnKhvK"
+    await client.send_message(client.get_channel("426680388585521163"), "{}".format(m))
+    server = client.get_server('426680388002250753')
+    await client.send_message(client.get_channel("429874952934785025"), ":large_blue_circle: `{}` joined the server! Now we have {} members.".format(userName, len(server.members)))
+    try:
+        await client.send_message(userName, "{}".format(m2))
+    except:
+        print("Join")
+
+@client.async_event
+async def on_member_remove(userName: discord.User):
+    leaves = ["Cya `{}` :wave:".format(userName)]
+    await client.send_message(client.get_channel("426680388585521163"), "{}".format(random.choice(leaves)))
+    print("Leave")
+    
+#EMOTES
+
+huglinks = ["https://i.imgur.com/yE2RnXK.gif",
+            "https://i.imgur.com/R9sYxk8.gif",
+            "https://i.imgur.com/iLBEoKv.gif",
+            "https://i.imgur.com/cc554e8.gif",
+            "https://i.imgur.com/1dqkjHe.gif",
+            "https://i.imgur.com/Ph8GTqg.gif",
+            "https://i.imgur.com/G6EnOxd.gif",
+            "https://i.imgur.com/ZxwHn5Y.gif",
+            "https://i.imgur.com/movPIsP.gif",
+            "https://i.imgur.com/tKlfSgo.gif",
+            "https://i.imgur.com/ICg9nCr.gif",
+            "https://i.imgur.com/yC95DC2.gif",
+            "https://i.imgur.com/hRYXNKX.gif",
+            "https://i.imgur.com/br3bGQc.gif",
+            "https://i.imgur.com/IcNGAQD.gif"]
+
+patlinks = ["https://i.imgur.com/8eApUKG.gif",
+            "https://i.imgur.com/qVcP9Pt.gif",
+            "https://i.imgur.com/X9hKO2p.gif",
+            "https://i.imgur.com/v8cRPH9.gif",
+            "https://i.imgur.com/N6C7C30.gif",
+            "https://i.imgur.com/M9QPcY6.gif",
+            "https://i.imgur.com/oUSdjX6.gif",
+            "https://i.imgur.com/mFFr4e0.gif",
+            "https://i.imgur.com/3F7kmCd.gif",
+            "https://i.imgur.com/7yFvJ6m.gif",
+            "https://i.imgur.com/y3La9yP.gif"]
+
+kisslinks = ["https://i.imgur.com/0Ri9sfq.gif",
+             "https://i.imgur.com/EMdpmXW.gif",
+             "https://i.imgur.com/Y9iLoiv.gif",
+             "https://i.imgur.com/ZlqZy8S.gif",
+             "https://i.imgur.com/ySav1IQ.gif",
+             "https://i.imgur.com/ZGfrn2d.gif",
+             "https://i.imgur.com/glwWeUl.gif",
+             "https://i.imgur.com/j5hDl7V.gif",
+             "https://i.imgur.com/w7mVYty.gif",
+             "https://i.imgur.com/FJ5bckO.gif",
+             "https://i.imgur.com/KqVmVU7.gif",
+             "https://i.imgur.com/EM1C9a6.gif",
+             "https://i.imgur.com/TACVpH9.gif",
+             "https://i.imgur.com/opiHLtc.gif",
+             "https://i.imgur.com/LylJAea.gif"]
+
+nomlinks = ["https://i.imgur.com/E1eQPfu.gif",
+            "https://i.imgur.com/UUZY3Rb.gif",
+            "https://i.imgur.com/Zd6fIpA.gif",
+            "https://i.imgur.com/i2NaBS7.gif",
+            "https://i.imgur.com/Up5J6Nn.gif",
+            "https://i.imgur.com/J5MLku7.gif",
+            "https://i.imgur.com/7yYgZXE.gif"]
+
+throwlinks = ["https://i.imgur.com/o9j2oNi.gif",
+              "https://i.imgur.com/wSb8aux.gif",
+              "https://i.imgur.com/QO8TrkK.gif",
+              "https://i.imgur.com/Ts3Cc52.gif",
+              "https://i.imgur.com/D3ggzfW.gif",
+              "https://i.imgur.com/eD5mE7R.gif",
+              "https://i.imgur.com/JCUipZJ.gif",
+              "https://i.imgur.com/VSg0YLw.gif",
+              "https://i.imgur.com/8mUufrm.gif"]
+
+bitelinks = ["https://i.imgur.com/E0jIIa9.gif",
+             "https://i.imgur.com/Nvkw6hN.gif",
+             "https://i.imgur.com/wr7l06j.gif",
+             "https://i.imgur.com/uce91VI.gif"]
+
+bloodsucklinks = ["https://i.imgur.com/UbaeYIq.gif",
+                  "https://i.imgur.com/qi83Eft.gif",
+                  "https://i.imgur.com/CtwmzpG.gif",
+                  "https://i.imgur.com/DAuEJ2F.gif",
+                  "https://i.imgur.com/By6IGzq.gif"]
+
+cuddlelinks = ["https://i.imgur.com/GWNWcLH.gif",
+               "https://i.imgur.com/i3Eqqgz.gif",
+               "https://i.imgur.com/GpFk6fE.gif",
+               "https://i.imgur.com/mc3Z7wf.gif",
+               "https://i.imgur.com/N5JYB5r.gif",
+               "https://i.imgur.com/PGp8JYq.gif"]
+
+highfivelinks = ["https://i.imgur.com/hjoQeOt.gif",
+                 "https://i.imgur.com/9nhheqT.gif",
+                 "https://i.imgur.com/yw3xMOu.gif",
+                 "https://i.imgur.com/Y4g5fsT.gif",
+                 "https://i.imgur.com/p6Hvx5r.gif",
+                 "https://i.imgur.com/33nuO9D.gif",
+                 "https://i.imgur.com/uFQnmYa.gif",
+                 "https://i.imgur.com/9KG3k2n.gif",
+                 "https://i.imgur.com/nHCC1ps.gif",
+                 "https://i.imgur.com/aKvaNba.gif",
+                 "http://i.imgur.com/hnHR29x.gif"]
+
+pokelinks = ["https://i.imgur.com/HAAktbl.gif",
+             "https://i.imgur.com/Fmd0Rsu.gif",
+             "https://i.imgur.com/1rObSM3.gif",
+             "https://i.imgur.com/Wo2fc94.gif",
+             "https://i.imgur.com/rtTucBW.gif",
+             "https://i.imgur.com/4c2mC5d.gif",
+             "https://i.imgur.com/1DVD84G.gif"]
+
+slaplinks = ["https://i.imgur.com/EAF42MG.gif",
+             "https://i.imgur.com/tLTT9Q4.gif",
+             "https://i.imgur.com/tEWjg7v.gif",
+             "https://i.imgur.com/MlkLTjv.gif",
+             "https://i.imgur.com/hoTYJZP.gif",
+             "https://i.imgur.com/Pthhs3Y.gif"]
+
+
+punchlinks = ["https://i.imgur.com/T2HdIv8.gif",
+              "https://i.imgur.com/LZz65rg.gif",
+              "https://i.imgur.com/FqPBIf3.gif",
+              "https://i.imgur.com/KmqPDQG.gif",
+              "https://i.imgur.com/yEx4aKu.gif"]
+
+starelinks = ["https://i.imgur.com/f8rFNH0.gif",
+              "https://i.imgur.com/ACCQDj4.gif",
+              "https://i.imgur.com/1Co1i9t.gif",
+              "https://i.imgur.com/uPZHQxV.gif",
+              "https://i.imgur.com/wXQLAb3.gif",
+              "https://i.imgur.com/hY7ZngK.gif"]
+
+facepalmlinks = ["http://media.giphy.com/media/8BYLSNmnJYQxy/giphy.gif",
+                 "https://uploads.disquscdn.com/images/84e9a7cef36a59ae605fad98c7ac567841be388820bf3fb936fd21b646a1d605.gif",
+                 "https://media1.tenor.com/images/74199573d51d1bd9b61029b611ee7617/tenor.gif?itemid=5695432",
+                 "http://i0.kym-cdn.com/photos/images/original/000/173/877/Facepalm.gif",
+                 "http://i.imgur.com/gXOcRsW.gif",
+                 "https://media.giphy.com/media/8cPpgUhTMjhF6/giphy.gif",
+                 "https://media1.tenor.com/images/a0282083ab6b592ab419659e4fb08624/tenor.gif?itemid=4745847"]
+
+crylinks = ["https://media1.giphy.com/media/ROF8OQvDmxytW/giphy-downsized.gif",
+            "https://media1.tenor.com/images/06ae6bbe852471939cf61a81e5a9eb23/tenor.gif?itemid=5370823",
+            "https://78.media.tumblr.com/e9fb46144efc579746e57bcaebd3350a/tumblr_olrmy4djBG1tydz8to1_500.gif",
+            "http://i.imgur.com/k5B1CBd.jpg",
+            "https://media.giphy.com/media/hyU0RHvlS3iQU/giphy.gif",
+            "https://media1.tenor.com/images/5912cbe4bc0dec511b5e0996a2ad9b6f/tenor.gif?itemid=8620704",
+            "https://s9.favim.com/orig/131225/an-anime-anime-gif-anime-guy-Favim.com-1182388.gif"]
+
+licklinks = ["https://i.imgur.com/QkRz1GJ.gif",
+             "https://i.imgur.com/ObCPKYV.gif",
+             "https://i.imgur.com/7fWrYqd.gif",
+             "https://i.imgur.com/O8CNVUL.gif",
+             "https://i.imgur.com/4QIlJtC.gif",
+             "https://i.imgur.com/LptJIi1.gif",
+             "https://i.imgur.com/THGgRJz.gif"]
+
+spanklinks = ["https://i.imgur.com/dt1TTQu.gif",
+              "https://i.imgur.com/ZsTbDvh.gif",
+              "https://i.imgur.com/4LHwG60.gif",
+              "https://i.imgur.com/xLOoHKP.gif",
+              "https://i.imgur.com/UShywVv.gif",
+              "https://i.imgur.com/RE3mnrA.gif"]
+
+# v!hug <user>
+@client.command(pass_context=True)
+async def hug(ctx, user: discord.Member = None):
+    author = ctx.message.author
+    msg = discord.Embed(colour=0x870099, description= "")
+    msg.title = ""
+    msg.set_footer(text=footer_text)
+    if user == None:
+        msg.add_field(name=error_img, value="Please mention someone you want to hug.")
+    else:
+        msg.set_image(url="{}".format(random.choice(huglinks)))
+        msg.add_field(name=":handshake: Interactions", value="<@{}> got a hug from <@{}>! Nawww.".format(user.id, author.id))
+    await client.say(embed=msg)
