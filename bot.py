6b59259679b2cd8cb05e17dc5e8b69a002ecd9b3
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-07-29 18:58:39 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -252,6 +252,7 @@ async def on_message(message):
         
     if message.content.lower().startswith('who is respect'):
         await client.send_message(message.channel, "Listen to this song/nhttps://www.youtube.com/watch?v=6EEW-9NDM5k")
+        
     else:
         await client.process_commands(msg)
             
