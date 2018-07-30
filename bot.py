4e1bddbd7d7ea47563eb08ed24e48394bd09cae6
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-07-30 20:14:31 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -294,6 +294,9 @@ async def on_message(message):
                         
     if message.content.lower().startswith('who is marcy'):
         await client.send_message(message.channel, "Who knows")
+                                
+    if message.content.lower().startswith('who is tredll'):
+        await client.send_message(message.channel, "Shut up. Shut the fuck up. Shut up! Shut the fuck up! Shut up! Shut up. This what I do, This what I do! This what I do. Shut the fuck up! Shut up! All you don't nobody goin' nowhere. Nobody goin' nowhere. Don't nobody going nowhere. You wanna stop? YOU WANNA STOP?! Stop. So if I get up somebody gon' hold 'im? Nobody touch me. Nobody touch me. Nobody touch me. You got blood on me, right? Got blood on me, right?! You got blood, use a tissue! You got-...You got blood on me? You got blood on me, right? Come on bro. Get your shit. What's up, bruh? Don't touch me. Don't touch me. You wanna fight, right? Don't touch me. Don't touch me. You wanna fight, right? Tell 'em turn the cameras off. Smack up. Don't say my name out on these cameras!")
         
     if message.content.lower().startswith('whalecum'):
         b = ["Stop saying that shit.", "just why", "(╯°□°）╯︵ ┻━┻", "boi", "fucking pervert"]
