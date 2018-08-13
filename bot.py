09ce41eb550b059fef05d354b1bd1c7b03a75b41
SibHuskie <38168951+SibHuskie@users.noreply.github.com>
2018-08-13 19:22:36 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1484,8 +1484,8 @@ async def mute(ctx, user: discord.Member = None):
             try:
                 await client.add_roles(user, rolename2)
                 msg.add_field(name=":speak_no_evil: ", value="<@{}> muted <@{}>.".format(author.id, user.id))
-    else:
-        msg.add_field(name=error_img, value="This command can only be used by Staff!")
+        else:
+            msg.add_field(name=error_img, value="This command can only be used by Staff!")
     await client.say(embed=msg)
 ##################################
 client.run(os.environ['BOT_TOKEN'])
